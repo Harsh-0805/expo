2c1b6b5c944adbc1366f1b92a420c2cdd6250f32
Harsh-0805 <harshbadala0805@gmail.com>
2024-01-06 10:23:38 +0530

    fix assests

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,40 +1,64 @@
-import React from 'react'
-import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle, CustomFlowbiteTheme } from 'flowbite-react';
+import React from "react";
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+  CustomFlowbiteTheme,
+} from "flowbite-react";
 import Logo from "../assets/logo.webp";
 import Ecell from "../assets/ecell-white-img.webp";
+import "./global.css";
 
 function Homepage() {
   return (
     <>
-    <div className="bg-[url('C:\Users\Lenovo\OneDrive\Documents\GitHub\expo\src\assets\frontpage.jpg')] bg-no-repeat bg-cover">
-    <Navbar fluid  className="bg-red-700">
-      <NavbarBrand>
-        <a href="https://www.ecellvnit.org/"><img src={Ecell} className="mr-3 h-6 sm:h-9"/></a>
-        <img src={Logo} className="mr-3 h-6 sm:h-9"  />
-      </NavbarBrand>
+      <div className="home bg-no-repeat bg-cover">
+        <Navbar fluid className="bg-red-700">
+          <NavbarBrand>
+            <a href="https://www.ecellvnit.org/">
+              <img src={Ecell} alt="logo" className="mr-3 h-6 sm:h-9" />
+            </a>
+            <img src={Logo} alt="logo" className="mr-3 h-6 sm:h-9" />
+          </NavbarBrand>
 
-      {/* <div className="flex md:order-2">
+          {/* <div className="flex md:order-2">
         <Button href="https://www.ecellvnit.org/" className="bg-black hover:bg-red-500">E-Cell</Button>
         <NavbarToggle />
       </div> */}
-      <NavbarCollapse>
-        <NavbarLink href="#" className='text-2xl'>Home
-        </NavbarLink>
-        <NavbarLink href="#" className='text-2xl'>About Expo</NavbarLink>
-        <NavbarLink href="#" className='text-2xl'>Timeline</NavbarLink>
-        <NavbarLink href="#" className='text-2xl'>Brochure</NavbarLink>
-        <NavbarLink href="#" className='text-2xl'>Contact Us</NavbarLink>
-      </NavbarCollapse>
-    </Navbar>
-    <div className=' h-screen'>
-      <div className='flex gap-10 absolute top-3/4 w-full justify-center'>
-      <Button className="w-2/12 px-8 py-4 bg-gradient-to-r from-red-500 to-red-800 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg ">Register</Button>
-      <Button className="w-2/12 px-8 py-4 bg-gradient-to-r from-red-500 to-red-800 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg ">Contact Us</Button>
+          <NavbarCollapse>
+            <NavbarLink href="#" className="text-2xl">
+              Home
+            </NavbarLink>
+            <NavbarLink href="#" className="text-2xl">
+              About Expo
+            </NavbarLink>
+            <NavbarLink href="#" className="text-2xl">
+              Timeline
+            </NavbarLink>
+            <NavbarLink href="#" className="text-2xl">
+              Brochure
+            </NavbarLink>
+            <NavbarLink href="#" className="text-2xl">
+              Contact Us
+            </NavbarLink>
+          </NavbarCollapse>
+        </Navbar>
+        <div className=" h-screen">
+          <div className="flex gap-10 absolute top-3/4 w-full justify-center">
+            <Button className="w-2/12 px-8 py-4 bg-gradient-to-r from-red-500 to-red-800 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg ">
+              Register
+            </Button>
+            <Button className="w-2/12 px-8 py-4 bg-gradient-to-r from-red-500 to-red-800 text-white font-bold rounded-full transition-transform transform-gpu hover:-translate-y-1 hover:shadow-lg ">
+              Contact Us
+            </Button>
+          </div>
+        </div>
       </div>
-    </div>
-    </div>  
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,53 +1,103 @@
 import React from "react";
+import "./global.css";
 
 const Timeline = () => {
-    return (
-        <>
-        <div className="text-white bg-[url('C:\Users\Lenovo\OneDrive\Documents\GitHub\expo\src\assets\red-grunge-background.jpg')]">
-          <ul className="p-4 lg:p-8 dark:bg-gray-200 dark:text-gray-100">
-	<li>
-    <h1 className="text-3xl text-red-500">TIMELINE OF EXPO'23</h1>
-		<article>
-			<a rel="noopener noreferrer"  className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-200">
-				<h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">REGISTRATION STARTS - </h3>
-				<time datetime="" className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400">January Start</time>
-				<p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">Startups will be evaluated on the basis of their market & customer base and their product showcasing capability.</p>
-			</a>
-		</article>
-	</li>
-	<li>
-		<article>
-			<a rel="noopener noreferrer"  className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900">
-				<h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">REGISTRATION ENDS - </h3>
-				<time datetime="" className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400">January End</time>
-				<p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">Deadline for the registration of startups for Expo.</p>
-			</a>
-		</article>
-	</li>
-	<li>
-		<article>
-			<a rel="noopener noreferrer"  className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900">
-				<h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">SIGNING OF MOU's - </h3>
-				<time datetime="" className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400">February start</time>
-				<p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">Shortlisted Startups will be signing the MOUs and further details will be communicated</p>
-			</a>
-		</article>
-	</li>
-	<li>
-		<article>
-			<a rel="noopener noreferrer"className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900">
-				<h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">LIVE EXHIBITION - </h3>
-				<time datetime="" className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400">1st week of FEB</time>
-				<p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">Awaiting Startups to showcase their amazing ideas and products.
-</p>
-			</a>
-		</article>
-	</li>
-</ul>
-</div>
-        </>
-    );
+  return (
+    <>
+      <div className="text-white timeline">
+        <ul className="p-4 lg:p-8 dark:bg-gray-200 dark:text-gray-100">
+          <li>
+            <h1 className="text-3xl text-red-500">TIMELINE OF EXPO'23</h1>
+            <article>
+              <a
+                rel="noopener noreferrer"
+                className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-200"
+              >
+                <h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">
+                  REGISTRATION STARTS -{" "}
+                </h3>
+                <time
+                  datetime=""
+                  className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
+                >
+                  January Start
+                </time>
+                <p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">
+                  Startups will be evaluated on the basis of their market &
+                  customer base and their product showcasing capability.
+                </p>
+              </a>
+            </article>
+          </li>
+          <li>
+            <article>
+              <a
+                rel="noopener noreferrer"
+                className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
+              >
+                <h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">
+                  REGISTRATION ENDS -{" "}
+                </h3>
+                <time
+                  datetime=""
+                  className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
+                >
+                  January End
+                </time>
+                <p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">
+                  Deadline for the registration of startups for Expo.
+                </p>
+              </a>
+            </article>
+          </li>
+          <li>
+            <article>
+              <a
+                rel="noopener noreferrer"
+                className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
+              >
+                <h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">
+                  SIGNING OF MOU's -{" "}
+                </h3>
+                <time
+                  datetime=""
+                  className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
+                >
+                  February start
+                </time>
+                <p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">
+                  Shortlisted Startups will be signing the MOUs and further
+                  details will be communicated
+                </p>
+              </a>
+            </article>
+          </li>
+          <li>
+            <article>
+              <a
+                rel="noopener noreferrer"
+                className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-gray-900"
+              >
+                <h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">
+                  LIVE EXHIBITION -{" "}
+                </h3>
+                <time
+                  datetime=""
+                  className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-gray-400"
+                >
+                  1st week of FEB
+                </time>
+                <p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-gray-300">
+                  Awaiting Startups to showcase their amazing ideas and
+                  products.
+                </p>
+              </a>
+            </article>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
-
 
 export default Timeline;
